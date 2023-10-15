fe4fa04d2b01d4c36219ae7db5e4cb491b82311b
James Barrett <james.p.barrett@gmail.com>
2023-10-15 12:49:15 +0100

    pc: a program counter

---
+++ rtl/pc.vhd
@@ -0,0 +1,59 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity pc is
+    port(
+        clock  : in  std_logic;
+        reset  : in  std_logic;
+        input  : in  std_logic_vector(15 downto 0);
+        set    : in  std_logic;
+        output : out std_logic_vector(15 downto 0)
+    );
+end entity pc;
+
+architecture rtl of pc is
+    signal pc_reg  : std_logic_vector(15 downto 0) := (others => '0');
+    signal pc_next : std_logic_vector(15 downto 0);
+
+    component addvec
+        generic(
+            N : integer := 16
+        );
+        port(
+            i0 : in  std_logic_vector(N - 1 downto 0);
+            i1 : in  std_logic_vector(N - 1 downto 0);
+            s  : out std_logic_vector(N - 1 downto 0);
+            co : out std_logic
+        );
+    end component;
+begin
+
+    output <= pc_reg;
+
+    process(clock)
+    begin
+        if rising_edge(clock) then
+            if not reset then
+                pc_reg <= (others => '0');
+            else
+                if set = '1' then
+                    pc_reg <= input;
+                else
+                    pc_reg <= pc_next;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    adder: addvec
+        generic map (
+            N => 16
+        )
+        port map (
+            i0 => pc_reg,
+            i1 => x"0001",
+            s  => pc_next,
+            co => open
+        );
+
+end rtl;
---
+++ rtl/pc_tb.vhd
@@ -0,0 +1,115 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity pc_tb is
+end entity pc_tb;
+
+architecture test of pc_tb is
+    signal clock : std_logic := '0';
+    signal reset : std_logic := '0';
+
+    signal input  : std_logic_vector(15 downto 0) := (others => 'X');
+    signal set    : std_logic := '0';
+    signal output : std_logic_vector(15 downto 0);
+
+    signal exp_output : std_logic_vector(15 downto 0) := (others => 'U');
+
+    component pc 
+        port(
+            clock  : in  std_logic;
+            reset  : in  std_logic;
+            input  : in  std_logic_vector(15 downto 0);
+            set    : in  std_logic;
+            output : out std_logic_vector(15 downto 0)
+        );
+    end component;
+
+    signal good : std_logic;
+
+    signal stop_sim : boolean := false;
+begin
+
+    UUT: pc
+        port map (
+            clock  => clock,
+            reset  => reset,
+            input  => input,
+            set    => set,
+            output => output
+        );
+
+    process begin
+        while not stop_sim loop
+            clock <= '0';
+            wait for 5 ns;
+            clock <= '1';
+            wait for 5 ns;
+        end loop;
+        wait;
+    end process;
+
+    stim: process 
+        variable n : integer range 0 to 65535;
+    begin
+        stop_sim <= false;
+        reset <= '0';
+        set   <= '0';
+
+        wait for 17 ns;
+
+
+        n := 0;
+        wait until rising_edge(clock);
+        exp_output <= std_logic_vector(to_unsigned(n, 16));
+        n := n + 1;
+        wait until falling_edge(clock);
+        assert good = '1';
+
+        reset <= '1';
+        set   <= '0';
+
+        for i in 0 to 255 loop
+            wait until rising_edge(clock);
+            exp_output <= std_logic_vector(to_unsigned(n, 16));
+            n := n + 1;
+
+            wait until falling_edge(clock);
+            assert good = '1';
+        end loop;
+
+        wait until falling_edge(clock);
+
+        n := 3467;
+        input <= std_logic_vector(to_unsigned(n, 16));
+        set <= '1';
+
+        wait until rising_edge(clock);
+
+        exp_output <= std_logic_vector(to_unsigned(n, 16));
+        n := n + 1;
+
+        wait until falling_edge(clock);
+
+        set <= '0';
+        assert good = '1';
+
+        for i in 0 to 255 loop
+            wait until rising_edge(clock);
+            exp_output <= std_logic_vector(to_unsigned(n, 16));
+            n := n + 1;
+
+            wait until falling_edge(clock);
+            assert good = '1';
+        end loop;
+
+        wait until falling_edge(clock);
+
+        wait for 10 ns;
+        stop_sim <= true;
+        wait;
+    end process stim;
+
+    good <= '1' when (output = exp_output) else '0';
+
+end architecture test;
